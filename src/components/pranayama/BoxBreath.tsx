abf1cbc610f97b864d75d7b776ca31c07441ebb3
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2018-08-14 13:39:24 +0200

    werk

--- src/components/pranayama/BoxBreath.tsx
+++ src/components/pranayama/BoxBreath.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View, Animated, Easing } from 'react-native'
+import { StyleSheet, Text, View, Animated } from 'react-native'
 import * as Pranayama from '../../lib/Pranayama'
-import { inflateSync } from 'zlib';
+
 
 interface Props {
   ratio: [number, number, number, number];
