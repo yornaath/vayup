2e87c0cec432562b63d3d295b1007a8fe9ce50e7
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2018-08-23 12:29:03 +0200

    started menu work

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Image, Dimensions } from 'react-native'
+import { StyleSheet, View, Image, Dimensions, Text, Animated } from 'react-native'
 import Breath from './containers/Breath'
 import {LinearGradient} from 'expo'
 
@@ -8,21 +8,36 @@ const screen = Dimensions.get("screen")
 
 export default class App extends React.Component {
 
+  state = {
+    menuOpen: true
+  }
+
+  animation = new Animated.Value(1)
+
   render() {
+
+    const screenOffset = this.animation.interpolate({
+      inputRange: [0,1],
+      outputRange: [screen.width - 200, 0]
+    })
+
     return (
-      <LinearGradient 
-        start={[0.1, 0.1]}
-        end={[1,1]}
-        colors={["rgb(255,255,255)", "rgb(235,235,235)"]}
-        style={styles.container}>
+      <View style={[styles.container]}>
+        
+          <View style={styles.contentContainer}>
 
-        <Image source={require("../assets/title_logo.png")} style={styles.logoTitle} resizeMode={"contain"}/>
+            <View style={styles.menuContainer}>
+              <Text>menu</Text>
+            </View>
 
-        <View style={styles.contentContainer}>
-          <Breath/>
-        </View>
+            <Animated.View style={[styles.screenContainer, {transform: [{ translateX: screenOffset }] }]}>
+              <Image source={require("../assets/title_logo.png")} style={styles.logoTitle} resizeMode={"contain"}/>
+              <Breath/>
+            </Animated.View>
+            
+          </View>
 
-      </LinearGradient>
+      </View>
     )
   }
 
@@ -44,7 +59,25 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
+    flexDirection: "row"
+  },
+  menuContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: screen.width,
+    height: screen.height
+  },
+  screenContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white'
   }
 })
